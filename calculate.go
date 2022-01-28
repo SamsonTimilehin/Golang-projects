59abf38a3b02d12e704379b197de7294265830a4
Samson Aderibigbe <samsontimilehin@gmail.com>
2022-01-28 23:36:53 +0100

    first commit

---
+++ calculate.go
@@ -0,0 +1,52 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+
+fmt.Println("Hey Welcome")
+
+var operator string
+var number1 int
+var number2 int
+
+fmt.Println("Enter first number")
+fmt.Scan(&number1)
+
+fmt.Println("Enter operator")
+fmt.Scan(&operator)
+
+fmt.Println("Enter second number")
+fmt.Scan(&number2)
+
+var result int
+
+if operator == "+" {
+    result = number1 + number2
+
+
+}else if operator == "-" {
+	result = number1 - number2
+	
+
+}else if operator == "*" {
+	result = number1 * number2
+	
+
+}else if operator == "/" {
+
+	if number2 == 0 {
+		fmt.Print(" second value cannot be zero", number2)
+	}else {
+
+	result = number1 / number2
+	
+
+	}
+}else {
+	fmt.Print("Invalid operator")
+}
+
+fmt.Printf("The result is %v",result)
+
+}
