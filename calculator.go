88ec06db8be670ad46c577c9485570ef4b9d53ea
Samson Aderibigbe <samsontimilehin@gmail.com>
2022-01-30 19:46:17 +0100

    update

--- calculator.go
+++ calculator.go
@@ -1,55 +1,70 @@
+
+// Simple Calculator with Golang
+
 package main
 import(
 	"fmt"
 )
 
-// Simple Calculator with Golang
+var symbol string
+	var num1 int
+	var num2 int
+
 
 func main(){
 
 fmt.Println("Hey Welcome")
 
-var operator string
-var number1 int
-var number2 int
-
-fmt.Println("Enter first number")
-fmt.Scan(&number1)
-
-fmt.Println("Enter operator")
-fmt.Scan(&operator)
-
-fmt.Println("Enter second number")
-fmt.Scan(&number2)
-
-var result int
-
-if operator == "+" {
-    result = number1 + number2
-
-
-}else if operator == "-" {
-	result = number1 - number2
+    
 	
+	collectUserInput()
+   calculate(num1,num2,symbol)
 
-}else if operator == "*" {
-	result = number1 * number2
-	
-
-}else if operator == "/" {
-
-	if number2 == 0 {
-		fmt.Print(" second value cannot be zero", number2)
-	}else {
-
-	result = number1 / number2
-	
-
-	}
-}else {
-	fmt.Print("Invalid operator")
 }
 
-fmt.Printf("The result is %v",result)
+func calculate(number1 int, number2 int, operator string){
+	
+	
+	var result int
+	
+	if operator == "+" {
+		result = number1 + number2
+	
+	
+	}else if operator == "-" {
+		result = number1 - number2
+		
+	
+	}else if operator == "*" {
+		result = number1 * number2
+		
+	
+	}else if operator == "/" {
+	
+		if number2 == 0 {
+			fmt.Print(" second value cannot be zero", number2)
+		}else {
+	
+		result = number1 / number2
+		
+	
+		}
+	}else {
+		fmt.Print("Invalid operator")
+	}
+	
+	fmt.Printf("The result is %v",result)
+	
+}
 
+func collectUserInput(){
+
+	fmt.Println("Enter first number")
+	fmt.Scan(&num1)
+	
+	fmt.Println("Enter operator")
+	fmt.Scan(&symbol)
+	
+	fmt.Println("Enter second number")
+	fmt.Scan(&num2)
 }
